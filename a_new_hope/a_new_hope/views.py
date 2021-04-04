7e83630930f769c7508f9c00dc81b8fa56e7a784
PabloGSC <pablo.sepulveda1@upr.edu>
2021-04-04 19:49:47 -0400

    Revert "Added SignUp/LogIn"
    This reverts commit 67e9f82e32b3b7a3c397a3fc5bc1af7ddf5b9ccf.

--- a_new_hope/a_new_hope/views.py
+++ a_new_hope/a_new_hope/views.py
@@ -35,23 +35,3 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
-
-@app.route('/register')
-def register():
-    """Renders the register page."""
-    return render_template(
-        'register.html',
-        title='Sign Up',
-        year=datetime.now().year,
-        message='Your application user register page.'
-    )
-
-@app.route('/login')
-def login():
-    """Renders the login page."""
-    return render_template(
-        'login.html',
-        title='Log In',
-        year=datetime.now().year,
-        message='Your application user login page.'
-    )
